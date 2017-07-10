8d4144096d2b80d8fdf7b6e19cb620da8efc40af
alespring <alespring@users.noreply.github.com>
2017-07-10 15:46:22 +0200

    Files actualised

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdlib.h>
 #include  <avr/io.h>
 
+#define LED1_AUS PORTD &= ~(1<<PD1); //löscht PD1
+#define LED1_AUS PORTD ¦= (1<<PD1); //letzt PD1
+
 int main(void)
 {
-
+	DDRD = 0xFF; // Port D als Ausgang setzen
+	PORTD = 0xFF;//alle Pins an Port D auf high setzen
+	
+	 LED1_AUS;
+	  
 	while(1)
 	{
+      
+	  
+	  
+	 
 
 	}// end of while
 
